b53efe986f29ebddbcf6cc3a024053077d9ee977
Kelvin Jones <kevinjones4453@gmail.com>
2019-01-24 12:38:38 -0500

    added tracking targets

--- js/scenes/HelloWorldSceneAR.js
+++ js/scenes/HelloWorldSceneAR.js
@@ -5,52 +5,105 @@ import React, { Component } from 'react';
 import {StyleSheet} from 'react-native';
 
 import {
-  ViroARScene,
-  ViroText,
-  ViroConstants,
+    ViroARScene,
+    ViroText,
+    ViroConstants,
+    ViroARImageMarker,
+    ViroBox,
+    ViroARTrackingTargets,
+    Viro360Image,
+    ViroMaterials,
+    ViroVideo
 } from 'react-viro';
 
 class HelloWorldSceneAR extends Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
-    };
+        // Set initial state here
+        this.state = {
+            text : "Initializing AR..."
+        };
 
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
+        // bind 'this' to functions
+        this._onInitialized = this._onInitialized.bind(this);
 
-  render() {
-    return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      </ViroARScene>
-    );
-  }
+        ViroMaterials.createMaterials({
+            grid: {
+                diffuseTexture: require('../../assets/images/apple.jpg'),
+            },
+            power: {
+                diffuseTexture: require('../../assets/images/powerade.png'),
+            },
+            shop: {
+                diffuseTexture: require('../../assets/images/kohls.jpg'),
+            }
+        });
 
-  _onInitialized(state, reason) {
-    if (state === ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text : "Hello World!"
-      });
-    } else if (state === ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+        ViroARTrackingTargets.createTargets({
+            "targetOne" : {
+                source : require('../../assets/images/apple.jpg'),
+                orientation : "Up",
+                physicalWidth : 0.1 // real world width in meters
+            },
+            "targetTwo" : {
+                source : require('../../assets/images/powerade.png'),
+                orientation : "Up",
+                physicalWidth : .05 // real world width in meters
+            },
+            "targetThree" : {
+                source : require('../../assets/images/kohls.jpg'),
+                orientation : "Up",
+                physicalWidth : .2 // real world width in meters
+            }
+        });
     }
-  }
+
+    render() {
+        return (
+            <ViroARScene onTrackingUpdated={this._onInitialized} >
+                <ViroARImageMarker target={"targetOne"} ><ViroBox position={[0, .5, 0]} scale={[.2, .2, .2]} materials={["grid"]} />
+                </ViroARImageMarker>
+                <ViroARImageMarker target={"targetTwo"} ><ViroBox position={[0, .5, 0]} scale={[.2, .2, .2]} materials={["power"]} />
+                </ViroARImageMarker>
+                {/*<ViroARImageMarker target={"targetThree"} ><ViroBox position={[0, .5, 0]} scale={[.2, .2, .2]} materials={["shop"]} />*/}
+                <ViroARImageMarker target={"targetThree"} >
+                    <ViroVideo
+                        source={require('../../assets/images/lights.mp4')}
+                        height={.2}
+                        width={.2}
+                        loop={true}
+                        position={[0,0,0]}
+                        materials={["shop"]}
+                       transformBehaviors={["billboardX"]}
+
+                    />
+                </ViroARImageMarker>
+            </ViroARScene>
+        );
+    }
+
+    _onInitialized(state, reason) {
+        if (state === ViroConstants.TRACKING_NORMAL) {
+            this.setState({
+                text : "Hello World!"
+            });
+        } else if (state === ViroConstants.TRACKING_NONE) {
+            // Handle loss of tracking
+        }
+    }
+
 }
 
 var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+    helloWorldTextStyle: {
+        fontFamily: 'Arial',
+        fontSize: 30,
+        color: '#ffffff',
+        textAlignVertical: 'center',
+        textAlign: 'center',
+    },
 });
 
 module.exports = HelloWorldSceneAR;
