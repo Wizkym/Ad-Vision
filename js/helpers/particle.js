b5648505cc846429130821a889dad8f42b94cc33
Wake <georgerincon2@gmail.com>
2019-01-28 04:51:51 -0500

    started Particle System helper dev

---
+++ js/helpers/particle.js
@@ -0,0 +1,24 @@
+'use strict';
+//A Particle System abstraction meant to speed up the process of implementing Particle Emiter to a desired scene.
+import React, {Component} from "react";
+
+import {
+    ViroARScene,
+    ViroConstants,
+    ViroARImageMarker,
+    ViroBox,
+    ViroARTrackingTargets,
+    ViroMaterials,
+    ViroVideo,
+    ViroParticleEmitter
+} from 'react-viro';
+
+class Particle extends Component{
+
+
+
+}
+
+
+export default Particle;
+
