5f73cd0b4c4faae48c3fd2af2960877fd12a607d
Wake <georgerincon2@gmail.com>
2019-01-29 19:52:23 -0500

    testing particle export

--- js/helpers/particle.js
+++ js/helpers/particle.js
@@ -1,24 +1,51 @@
 'use strict';
 //A Particle System abstraction meant to speed up the process of implementing Particle Emiter to a desired scene.
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import {
-    ViroARScene,
-    ViroConstants,
-    ViroARImageMarker,
-    ViroBox,
-    ViroARTrackingTargets,
-    ViroMaterials,
-    ViroVideo,
+
     ViroParticleEmitter
 } from 'react-viro';
 
-class Particle extends Component{
+//Testing JSX
 
+export function Fountain([...coordinates], duration, source) {
 
+    let photo = `${source}`;
 
+    return (<ViroParticleEmitter
+        position={coordinates}
+        duration={duration}
+        run={true}
+
+        image={{
+            source: require("../res/particles/" + photo),
+            height: 0.1,
+            width: 0.1,
+        }}
+    />
+    );
 }
 
 
-export default Particle;
+
+/*         reactFountain = ([...coordiantes], duration, autoStart, source) => {
+        return (<ViroParticleEmitter
+            position={coordiantes}
+            duration={duration}
+            run={autoStart || true}
+
+            image={{
+                source:require(`./res/particles/${source}`),
+                height:0.1,
+                width:0.1,
+            }}
+        />
+        );
+    } */
+
+
+
+
+
 
