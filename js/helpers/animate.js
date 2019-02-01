5a041979efd62065270566342b718458b9a35181
Wake <georgerincon2@gmail.com>
2019-01-31 23:10:51 -0500

    testing anim transition workaround

--- js/helpers/animate.js
+++ js/helpers/animate.js
@@ -8,17 +8,103 @@ export default {
     registerAll() {
 
         ViroAnimations.registerAnimations({
-            animateScale: {
+            animateOpacity1: {
                 properties: {
-                    scaleX: 1.0, scaleY: 1.0, scaleZ: 1.0,
+                
+                    opacity: 0.1
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity2: {
+                properties: {
+                
+                    opacity: 0.2
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity3: {
+                properties: {
+                
+                    opacity: 0.3
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity4: {
+                properties: {
+                
+                    opacity: 0.4
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity5: {
+                properties: {
+                
+                    opacity: 0.5
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity6: {
+                properties: {
+                
+                    opacity: 0.6
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity7: {
+                properties: {
+                
+                    opacity: 0.7
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity8: {
+                properties: {
+                
+                    opacity: 0.8
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity9: {
+                properties: {
+                
+                    opacity: 0.9
+                },
+
+                easing:"Linear", 
+                duration:100
+            },
+            animateOpacity10: {
+                properties: {
+                
                     opacity: 1.0
                 },
-                easing:"Linear", 
-                duration: 5000
-            },
-        });
 
-        console.log("animations have been registered");
+                easing:"Linear", 
+                duration:100
+            },
+            
+            rotate:{properties:{rotateZ:"+=360"}, duration:5000},
+            plswrk:[
+                ["animateOpacity1","animateOpacity2","animateOpacity3","animateOpacity4","animateOpacity5","animateOpacity6","animateOpacity7", "animateOpacity8",
+                "animateOpacity9", "animateOpacity10",   "rotate"],
+            ]
+        });
 
 
     }
