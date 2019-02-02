a6551be9e61cf270ab3f62bc265ef6977f2c7165
Kelvin Jones <kevinjones4453@gmail.com>
2019-02-02 16:02:14 -0500

    successful test with image detection

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// Cloud Vision setup
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+router.get("/", (req, res) => {
+    detectText()
+        .then((data) => {
+            res.send(data);
+        });
+});
+
+async function detectText() {
+    // [START vision_text_detection]
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const fileName = 'powerade.png';
+
+    // Performs text detection on the local file
+    const [result] = await client.textDetection(fileName);
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    detections.forEach(text => console.log(text));
+    // [END vision_text_detection]
+    return detections;
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
 const app = express();
+const routes = require('./routes');
 
 // Middleware
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './src')));
+app.use(routes);
+
 
 const PORT = process.env.PORT || 3085;
 
+// Start the API server
 app.listen(PORT, err => {
     if (err) {
         console.error(err);
     } else {
-        console.log(`App listen to port: ${PORT}`);
+        console.log(`API Server now listening on PORT: ${PORT}`);
     }
 });
+
