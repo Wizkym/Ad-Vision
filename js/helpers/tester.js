277b98969201da7a20dfb0f21068c39e41029417
Wake <georgerincon2@gmail.com>
2019-02-05 15:58:24 -0500

    testing new animation and anchoring logic

--- js/helpers/tester.js
+++ js/helpers/tester.js
@@ -1,57 +1,46 @@
 
 import React from "react";
-
 import particle from '../helpers/particle'; 
-
+import {targets} from '../../models/trackingTargets';
 import {
     ViroARImageMarker,
     ViroBox,
-    ViroARTrackingTargets,
 
 } from 'react-viro';
 
-
-
-export const runTestEmitter = () =>{
+export function runTestEmitter () {
     particle.Firework([0, 0, 0], 4200, "fxparttinyglowy.png", false, 1800)
 
 }
 export function Target(type, name) {
 
-    let dummyKey = dummyKeyGen();
+    //let dummyKey = dummyKeyGen();
     //alert(dummyKey);
 
     if (type === "box") {
-
-        renderables.data.push(
-            <ViroARImageMarker target={name} key={dummyKey} >
+        renderables.data.push(name);
+          /*   { <ViroARImageMarker target={name} key={dummyKey} >
                 <ViroBox position={[0, 0, 0]}
                     opacity={0.0}
                     scale={[0.0, 0.0, 0.0]}
                     materials={["apple"]}
                     animation={{
-                        name: 'plswrk', delay: 3000, run: true
+                        name: 'plswrk', delay: 3000, run: true,
+                        onStart:{runTestEmitter}
                     }} />
-            </ViroARImageMarker>
-        );
-
-
-
+            </ViroARImageMarker> } */
         if (!renderables.hasBeenFilled) {
             renderables.hasBeenFilled = true;
         }
-
-
-
     }
 
-
+    //alert(JSON.stringify(renderables.data));
 }
-const dummyKeyGen = () => {
+/* const dummyKeyGen = () => {
     let rnd  = Math.floor(Math.random() * 900000) + 100000;
     return rnd;
 
-}
+} */
 
 export let renderables = {
     data: [],
