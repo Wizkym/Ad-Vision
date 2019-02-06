c447b965ab97cb717cafdae4c9cc0778c5f31a81
Kelvin Jones <kevinjones4453@gmail.com>
2019-02-05 20:58:07 -0500

    screenshots good to go

--- src/js/scenes/Landing.js
+++ src/js/scenes/Landing.js
@@ -9,7 +9,10 @@ import {
 
 import Customize from './Customize';
 import styles from '../../assets/styles/styles';
-import { ViroARSceneNavigator } from 'react-viro';
+import renderIf from '../helpers/renderIf';
+import {
+    ViroARSceneNavigator,
+    ViroConstants} from 'react-viro';
 import * as Animatable from 'react-native-animatable';
 /*
  TODO: Insert your API key below
@@ -23,6 +26,7 @@ const ARScene = require('./VisionAR');
 const UNSET = "UNSET";
 const AR_NAVIGATOR_TYPE = "AR";
 const LOGIN_NAVIGATOR_TYPE ="LOGIN";
+const SHARE = 'SHARE';
 
 // This determines which type of experience to launch in, or UNSET, if the user should
 // be presented with a choice of AR or VR. By default, we offer the user a choice.
@@ -35,19 +39,27 @@ export default class Landing extends Component {
             navigator : defaultNavigatorType,
             sharedProps : sharedProps,
             hits: [],
+            haveSavedMedia: false,
             noneChecked: false,
             homeIsChecked: false,
             artIsChecked: false,
             techIsChecked: false,
-            educationIsChecked: false
+            educationIsChecked: false,
+            photoPermission: '',
+            cameraPermission: '',
+            screenshot_count: 0,
+            imgUrl: ''
         };
 
         this._getExperienceSelector = this._getExperienceSelector.bind(this);
         this._getARNavigator = this._getARNavigator.bind(this);
         this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
         this._exitViro = this._exitViro.bind(this);
+        this._renderShareScreen = this._renderShareScreen.bind(this);
+        this._setARNavigatorRef = this._setARNavigatorRef.bind(this);
         this._checkBoxText =this._checkBoxText.bind(this);
         this._handlePress = this._handlePress.bind(this);
+        this._takeScreenShot = this._takeScreenShot.bind(this);
     }
 
     // Conditional rendering
@@ -56,8 +68,10 @@ export default class Landing extends Component {
             return this._getExperienceSelector();
         }  else if (this.state.navigator === LOGIN_NAVIGATOR_TYPE) {
             return this._getCustomizeScreen();
-        }else if (this.state.navigator === AR_NAVIGATOR_TYPE) {
+        } else if (this.state.navigator === AR_NAVIGATOR_TYPE) {
             return this._getARNavigator();
+        } else if (this.state.navigator === SHARE) {
+            return this._renderShareScreen();
         }
     }
 
@@ -109,7 +123,9 @@ export default class Landing extends Component {
         return (
             <View style={{flex: 1}}>
                 <ViroARSceneNavigator {...this.state.sharedProps}
-                                      initialScene={{scene: ARScene}}/>
+                                      initialScene={{scene: ARScene}}
+                                      ref={this._setARNavigatorRef}
+                />
                 <View style={{position: 'absolute', backgroundColor:"#ffffff22", left: 30, right: 30, top: 30, alignItems: 'center'}}>
                     <Text style={{fontSize:12, color:"#ffffff"}}>Tracking initialized.</Text>
                 </View>
@@ -120,9 +136,25 @@ export default class Landing extends Component {
                         <Image source={require ('../res/icon_left_w.png')} style={{height: 30, width: 40}}/>
                     </TouchableHighlight>
                 </View>
+
+                <View style={{position: 'absolute', left: 175, right: 0, bottom: 15}}>
+                    <TouchableHighlight style={styles.back}
+                                        onPress={() => {this._takeScreenShot()}}
+                                        underlayColor={'#00000000'}>
+                        <Image source={require('../res/icon_360_w.png')} style={{height: 30, width: 40}}/>
+                    </TouchableHighlight>
+                </View>
             </View>
         );
     }
+
+    // Share Screen
+    _renderShareScreen() {
+        return(
+            <Image source={{uri:this.state.imgUrl}} style={styles.backgroundImage} resizeMethod={'resize'} />
+        )
+    }
+
 
     // Returns the Customize Screen
     _getCustomizeScreen = () => (
@@ -136,6 +168,33 @@ export default class Landing extends Component {
                    _handlePress={this._handlePress}
         />
     );
+
+    // Helper function called while initializing <ViroARSceneNavigator>
+    _setARNavigatorRef(ARNavigator){
+        this._arNavigator = ARNavigator;
+    }
+
+    // Screenshots
+    _takeScreenShot() {
+        this._arNavigator.sceneNavigator.takeScreenshot("vision_still_" + this.state.screenshot_count, false)
+            .then((retDict)=>{
+                if (!retDict.success) {
+                    if (retDict.errorCode === ViroConstants.RECORD_ERROR_NO_PERMISSION) {
+                        alert("Please allow camera permissions!");
+                    }
+                }
+                let currentCount = this.state.screenshot_count + 1;
+                this.setState({
+                    imgUrl: "file://" + retDict.url,
+                    screenshot_count: currentCount,
+                    navigator : SHARE
+                });
+                alert('Screenshot Check');
+            })
+            .catch(err => {
+                alert('This dont work!');
+            });
+    }
 
     // This function returns an anonymous/lambda function to be used
     // by the experience selector buttons
@@ -187,7 +246,10 @@ export default class Landing extends Component {
 
     // Google Cloud Vision function
     _handlePress = async () => {
-        fetch('http://192.168.1.85:3085/', {method: 'GET'})
+        /*
+            TODO: Insert your ip address below
+        */
+        fetch('http://10.136.13.80/', {method: 'GET'})
             .then(response => response.json())
             .then((response) => alert(response.description))
             .catch(err => alert(err));
