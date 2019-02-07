a2eba5db1c102b9b23567d147d93e5118b3bb57c
DavidDBaker <daviiddamiianbaker@gmail.com>
2019-02-07 02:43:49 -0500

    query function done

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,7 @@ const request = require('request');
 const apiKey = '###';
 const customConfigId = '###';
 
-function searchQuery() {
+function searchQuery(query) {
   let options = {
     url:
       `https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?q=${encodeURIComponent(query)}&customconfig=###`,
@@ -14,15 +14,16 @@ function searchQuery() {
       'Ocp-Apim-Subscription-Key' : 'e8d2a1ed1c8f49639d03b3f7eff8d85d'
     }
   };
+  request(options, (error, response, body) =>{
+    let searchResponse = JSON.parse(body);
+    for(let i = 0; i < searchResponse.webPages.value.length; ++i){
+      let webPage = searchResponse.webPages.value[i];
+      console.log('name: ' + webPage.name);
+      console.log('displayUrl: ' + webPage.displayUrl);
+      console.log();
+    }
+  });  
 }
 
-request(options, (error, response, body) =>{
-  let searchResponse = JSON.parse(body);
-  for(let i = 0; i < searchResponse.webPages.value.length; ++i){
-    let webPage = searchResponse.webPages.value[i];
-    console.log('name: ' + webPage.name);
-    console.log('displayUrl: ' + webPage.displayUrl);
-    console.log();
-  }
-});
-
+const query = process.arg[2] || 'powerade';
+searchQuery(query);
